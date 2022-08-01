7a51bbd6595ca8e8e417a3ce7d94f549b302ab11
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2022-07-31 23:30:34 -0700

    Fix 404 page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/pythagorean", (req, res) => {
   res.sendFile(`${__dirname}/html/pythagorean.html`);
 });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.status(404).send("<h1>404</h1>");
 });
 
